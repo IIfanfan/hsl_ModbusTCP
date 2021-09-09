eb14de94062c1983fbe3641adfffd9fa65dedfba
范天亮 <1092924979@qq.com>
2021-09-09 17:57:15 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace HslDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.LoglistBox = new System.Windows.Forms.ListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 565);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1083, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(131, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // LoglistBox
+            // 
+            this.LoglistBox.FormattingEnabled = true;
+            this.LoglistBox.HorizontalScrollbar = true;
+            this.LoglistBox.ItemHeight = 12;
+            this.LoglistBox.Location = new System.Drawing.Point(103, 6);
+            this.LoglistBox.Name = "LoglistBox";
+            this.LoglistBox.ScrollAlwaysVisible = true;
+            this.LoglistBox.Size = new System.Drawing.Size(987, 508);
+            this.LoglistBox.TabIndex = 25;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(5, 102);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 24;
+            this.button3.Text = "自动化抓取";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(5, 61);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 23;
+            this.button2.Text = "更新右托盘";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(5, 20);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 22;
+            this.button1.Text = "更新左托盘";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 527);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 23);
+            this.button4.TabIndex = 26;
+            this.button4.Text = "读取更新左托盘";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(137, 527);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(117, 23);
+            this.button5.TabIndex = 27;
+            this.button5.Text = "读取更新右托盘";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(283, 527);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(141, 23);
+            this.button6.TabIndex = 28;
+            this.button6.Text = "读取自动化抓取";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(430, 527);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(141, 23);
+            this.button7.TabIndex = 29;
+            this.button7.Text = "读取更新子弹";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(5, 143);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 30;
+            this.button8.Text = "更新子弹";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(5, 203);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 23);
+            this.button9.TabIndex = 31;
+            this.button9.Text = "放料完成";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(5, 241);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(75, 23);
+            this.button10.TabIndex = 32;
+            this.button10.Text = "抓取次数";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(5, 174);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(75, 23);
+            this.button11.TabIndex = 31;
+            this.button11.Text = "放料完成";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(797, 527);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(193, 23);
+            this.button12.TabIndex = 33;
+            this.button12.Text = "自动测试抓取和放料";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1083, 587);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.LoglistBox);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.statusStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ListBox LoglistBox;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,304 @@
+﻿using HslCommunication;
+using HslCommunication.ModBus;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HslDemo
+{
+    public partial class Form1 : Form
+    {
+        private ModbusTcpNet busTcpClient = null;
+        public static String m_sIP = "127.0.0.1";//ip设置 
+        public static int m_iPort = 502;//端口号
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            busTcpClient = new ModbusTcpNet(m_sIP, m_iPort, 1);
+            busTcpClient.AddressStartWithZero = true;
+            busTcpClient.DataFormat = HslCommunication.Core.DataFormat.CDAB;
+
+            try
+            {
+                OperateResult connect = busTcpClient.ConnectServer();
+                if (connect.IsSuccess)
+                {
+                    toolStripStatusLabel1.Text = "连接成功！";
+                }
+                else
+                {
+                    toolStripStatusLabel1.Text = "连接失败！";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show( ex.Message );
+            }
+           // timer1.Enabled = true;
+        }
+               /// <summary>
+        /// 统一的数据写入的结果显示
+        /// </summary>
+        /// <param name="result"></param>
+        /// <param name="address"></param>
+        private void writeResultRender( OperateResult result, string address )
+        {
+            if (result.IsSuccess)
+            {
+                writeListBox( DateTime.Now.ToString( "[HH:mm:ss] " )+":"+address + "写入成功" );
+            }
+            else
+            {
+                 writeListBox( DateTime.Now.ToString( "[HH:mm:ss] " )+":"+address +"写入失败"+ result.ToMessageShowString());
+            }
+        }
+          /// <summary>
+        /// 统一的读取结果的数据解析，显示
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="result"></param>
+        /// <param name="address"></param>
+        /// <param name="textBox"></param>
+        private void readResultRender<T>( OperateResult<T> result, string address)
+        {
+            if (result.IsSuccess)
+            {
+                writeListBox( DateTime.Now.ToString( "[HH:mm:ss] " ) + ":"+address + "读取成功" +result.Content);
+            }
+            else
+            {
+                writeListBox( DateTime.Now.ToString( "[HH:mm:ss] " ) + ":"+address + "读取失败" +result.ToMessageShowString());
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // float写入
+            //0,更新左托盘;2,X坐标；4，Y坐标；6，Z坐标；8，角度Z，10，角度Y；12，角度Z；
+            string strAddr1 = "0";
+            string strValue1 = "0";
+            string strAddr2 = "2";
+            string strValue2 = "-2051.399";
+            string strAddr3 = "4";
+            string strValue3 = "-868.407";
+            string strAddr4 = "6";
+            string strValue4 = "761.177";
+            string strAddr5 = "8";
+            string strValue5 = "0.008387";
+            string strAddr6 = "10";
+            string strValue6 = "-0.999730";
+            string strAddr7 = "12";
+            string strValue7 = "0.017084";
+            try
+            {
+                 writeResultRender(busTcpClient.Write(strAddr1, float.Parse(strValue1)), strAddr1);
+                 writeResultRender(busTcpClient.Write(strAddr2, float.Parse(strValue2)), strAddr2);
+                 writeResultRender(busTcpClient.Write(strAddr3, float.Parse(strValue3)), strAddr3);
+                 writeResultRender(busTcpClient.Write(strAddr4, float.Parse(strValue4)), strAddr4);
+                 writeResultRender(busTcpClient.Write(strAddr5, float.Parse(strValue5)), strAddr5);
+                 writeResultRender(busTcpClient.Write(strAddr6, float.Parse(strValue6)), strAddr6);
+                 writeResultRender(busTcpClient.Write(strAddr7, float.Parse(strValue7)), strAddr7);
+            }
+            catch(Exception ex)
+            {
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // float写入
+            //0,更新右托盘;2,X坐标；4，Y坐标；6，Z坐标；8，角度Z，10，角度Y；12，角度Z；
+            string strAddr1 = "0";
+            string strValue1 = "1";
+            string strAddr2 = "2";
+            string strValue2 = "-2051.399";
+            string strAddr3 = "4";
+            string strValue3 = "-868.407";
+            string strAddr4 = "6";
+            string strValue4 = "761.177";
+            string strAddr5 = "8";
+            string strValue5 = "0.008387";
+            string strAddr6 = "10";
+            string strValue6 = "-0.999730";
+            string strAddr7 = "12";
+            string strValue7 = "0.017084";
+            try
+            {
+                writeResultRender(busTcpClient.Write(strAddr1, float.Parse(strValue1)), strAddr1);
+                writeResultRender(busTcpClient.Write(strAddr2, float.Parse(strValue2)), strAddr2);
+                writeResultRender(busTcpClient.Write(strAddr3, float.Parse(strValue3)), strAddr3);
+                writeResultRender(busTcpClient.Write(strAddr4, float.Parse(strValue4)), strAddr4);
+                writeResultRender(busTcpClient.Write(strAddr5, float.Parse(strValue5)), strAddr5);
+                writeResultRender(busTcpClient.Write(strAddr6, float.Parse(strValue6)), strAddr6);
+                writeResultRender(busTcpClient.Write(strAddr7, float.Parse(strValue7)), strAddr7);
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //0,抓取;1，抓取金手指
+            string strAddr1 = "7";
+            string strValue1 = "11";
+            string strAddr2 = "5";
+            string strValue2 = "1111";
+            try
+            {
+                writeResultRender(busTcpClient.Write(strAddr1, float.Parse(strValue1)), strAddr1);
+                writeResultRender(busTcpClient.Write(strAddr2, float.Parse(strValue2)), strAddr2);
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+        /// <summary>
+        /// log写入listbox控件
+        /// </summary>
+        /// <param name="s"></param>
+        public void writeListBox(string s)
+        {
+            string stringLog = System.DateTime.Now.ToString() + "-->:" + s;
+            this.Invoke((MethodInvoker)delegate
+            {
+                if (LoglistBox.Items.Count >= 50)
+                    LoglistBox.Items.Clear();
+                LoglistBox.Items.Add(stringLog);
+            });
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            //读取更新右托盘
+            string strAddr1 = "0";
+            string strAddr2 = "14";
+            readResultRender(busTcpClient.ReadFloat(strAddr1), strAddr1);
+            readResultRender(busTcpClient.ReadFloat(strAddr2), strAddr2);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+             // 读取float变量 
+            string strAddr1 = "0";
+            string strAddr2 = "2";
+            string strAddr3 = "4";
+            string strAddr4 = "6";
+            string strAddr5 = "8";
+            string strAddr6 = "10";
+            string strAddr7 = "12";
+            readResultRender( busTcpClient.ReadFloat(strAddr1), strAddr1);
+            readResultRender(busTcpClient.ReadFloat(strAddr2), strAddr2);
+            readResultRender(busTcpClient.ReadFloat(strAddr3), strAddr3);
+            readResultRender(busTcpClient.ReadFloat(strAddr4), strAddr4);
+            readResultRender(busTcpClient.ReadFloat(strAddr5), strAddr5);
+            readResultRender(busTcpClient.ReadFloat(strAddr6), strAddr6);
+            readResultRender(busTcpClient.ReadFloat(strAddr7), strAddr7);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //读取更新右托盘
+            string strAddr1 = "0";
+            string strAddr2 = "2";
+            string strAddr3 = "4";
+            string strAddr4 = "6";
+            string strAddr5 = "8";
+            string strAddr6 = "10";
+            string strAddr7 = "12";
+            readResultRender(busTcpClient.ReadFloat(strAddr1), strAddr1);
+            readResultRender(busTcpClient.ReadFloat(strAddr2), strAddr2);
+            readResultRender(busTcpClient.ReadFloat(strAddr3), strAddr3);
+            readResultRender(busTcpClient.ReadFloat(strAddr4), strAddr4);
+            readResultRender(busTcpClient.ReadFloat(strAddr5), strAddr5);
+            readResultRender(busTcpClient.ReadFloat(strAddr6), strAddr6);
+            readResultRender(busTcpClient.ReadFloat(strAddr7), strAddr7);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //busTcpClient.Write("20", float.Parse("1"));
+            //string strAddr3 = "20";
+            //writeResultRender(busTcpClient.Write(strAddr3, float.Parse(strAddr3)),strAddr3);
+            //if (busTcpClient.ReadFloat(strAddr3).IsSuccess)
+            //{
+            //    toolStripStatusLabel1.Text = "连接成功！";
+            //}
+            //else
+            //{
+            //    toolStripStatusLabel1.Text = "连接失败！";
+            //}
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            busTcpClient.ConnectClose();
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            //抓取次数
+            string strAddr1 = "7";
+            float ftimes = 1;
+            writeResultRender(busTcpClient.Write(strAddr1, ftimes), strAddr1);
+            ftimes++;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            //放料完成
+            string strAddr1 = "11";
+            float ftimes = 11;
+            writeResultRender(busTcpClient.Write(strAddr1, ftimes), strAddr1);
+            
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            //开始放料
+            string strAddr1 = "9";
+            float ftimes = 11;
+            writeResultRender(busTcpClient.Write(strAddr1, ftimes), strAddr1);
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 26; i++)
+            {
+                //开始抓
+                button3_Click(null,null);
+                //清零寄存器
+                string strAddr1 = "7";
+                string strValue1 = "0";
+                writeResultRender(busTcpClient.Write(strAddr1, float.Parse(strValue1)), strAddr1);
+                //等待放
+                button11_Click(null, null);
+                 strAddr1 = "9";
+                 strValue1 = "0";
+                writeResultRender(busTcpClient.Write(strAddr1, float.Parse(strValue1)), strAddr1);
+                //放完
+                button9_Click(null, null);
+                strAddr1 = "9";
+                strValue1 = "0";
+                writeResultRender(busTcpClient.Write(strAddr1, float.Parse(strValue1)), strAddr1);
+            }
+        }
+    }
+}
